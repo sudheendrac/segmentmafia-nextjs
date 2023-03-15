7a621cc163fa95eb3d7a839ed8bdd6e437b36c43
sudheendrac <127924187+sudheendrac@users.noreply.github.com>
2023-03-14 23:21:37 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,12 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 
-
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Segment Mafia</title>
-    <p> Former Segment employees who are now running their own startups or leading teams elsewhere  </p>
+   // <p> Former Segment employees who are now running their own startups or leading teams elsewhere  </p>
         <link rel="icon" href="assets/Segment_Logo_Avatar_Green.eps" />
       </Head>
 
