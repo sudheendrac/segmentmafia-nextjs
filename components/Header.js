a2246f6fd7e73b44bdd4f30786d89f3282c7c667
sudheendrac <127924187+sudheendrac@users.noreply.github.com>
2023-03-17 10:20:31 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ export default function Header({ title }) {
     <>
       <h1 className="title">{title}</h1>
       <p>
-  A website featuring former Segmenters who have started their own companies or leading teams at notable organizations.
+  A website featuring former Segmenters who have started their own companies or leading teams at notable startups.
       </p>
     </>
   );
